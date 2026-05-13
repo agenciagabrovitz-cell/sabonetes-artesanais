f747dd03c1f447197a9d7e69b66007d0a7f29720
gabrovitzmetaads-alt <gabrovitzmetaads@gmail.com>
2026-05-12 21:01:46 -0300

    Fix: Reconstruídos divisores SVG com coordenadas absolutas para preenchimento total garantido

--- src/App.jsx
+++ src/App.jsx
@@ -190,9 +190,9 @@ function App() {
         </FadeUp>
         
         {/* Divisor SVG - Onda Suave para Branco */}
-        <div className="absolute bottom-[-1px] left-[-0.5%] w-[101%] overflow-hidden leading-[0] transform rotate-180 z-10">
-          <svg className="relative block w-full h-[80px] scale-x-105" viewBox="0 0 1200 120" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5,73.84-4.36,147.54,16.88,218.2,35.26,69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113,14.29,1200,52.47V0Z" fill="#ffffff"></path>
+        <div className="absolute bottom-[-2px] left-[-2%] w-[104%] overflow-hidden leading-[0] transform rotate-180 z-20">
+          <svg className="relative block w-full h-[100px] scale-x-110" viewBox="0 0 1200 120" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0,0 C300,120 900,0 1200,40 V0 H0 Z" fill="#ffffff" transform="rotate(180 600 60)"></path>
           </svg>
         </div>
       </section>
@@ -215,9 +215,9 @@ function App() {
         </FadeUp>
 
         {/* Divisor SVG - Curva Suave para Preto */}
-        <div className="absolute bottom-[-1px] left-[-0.5%] w-[101%] overflow-hidden leading-[0] z-10">
-          <svg className="relative block w-full h-[100px] scale-x-105" viewBox="0 0 1200 120" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5V120H0Z" fill="#0D0D0D"></path>
+        <div className="absolute bottom-[-2px] left-[-2%] w-[104%] overflow-hidden leading-[0] z-20">
+          <svg className="relative block w-full h-[110px] scale-x-110" viewBox="0 0 1200 120" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0,40 C400,0 800,120 1200,40 V120 H0 Z" fill="#0D0D0D"></path>
           </svg>
         </div>
       </section>
@@ -353,9 +353,9 @@ function App() {
       </section>
 
       {/* Divisor SVG - Transição Suave para Cinza Facebook */}
-      <div className="w-[101%] left-[-0.5%] overflow-hidden leading-[0] bg-[#111] z-10 relative">
-        <svg className="relative block w-full h-[80px] scale-x-105" viewBox="0 0 1200 120" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V120H0Z" fill="#f0f2f5"></path>
+      <div className="w-[104%] left-[-2%] overflow-hidden leading-[0] bg-[#111] z-20 relative">
+        <svg className="relative block w-full h-[100px] scale-x-110" viewBox="0 0 1200 120" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0,40 C300,120 900,0 1200,40 V120 H0 Z" fill="#f0f2f5"></path>
         </svg>
       </div>
 
@@ -435,11 +435,9 @@ function App() {
           </FadeUp>
 
         {/* Divisor SVG - Onda Elegante para Oferta */}
-        <div className="absolute bottom-[-1px] left-[-0.5%] w-[101%] overflow-hidden leading-[0] z-10">
-          <svg className="relative block w-full h-[80px] scale-x-105" viewBox="0 0 1200 120" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5,73.84-4.36,147.54,16.88,218.2,35.26,69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113,14.29,1200,52.47V0Z" opacity=".25" fill="#e8006f"></path>
-            <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5V0Z" opacity=".5" fill="#e8006f"></path>
-            <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" fill="#0a0a0a"></path>
+        <div className="absolute bottom-[-2px] left-[-2%] w-[104%] overflow-hidden leading-[0] z-20">
+          <svg className="relative block w-full h-[100px] scale-x-110" viewBox="0 0 1200 120" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0,40 C300,120 900,0 1200,40 V120 H0 Z" fill="#0a0a0a"></path>
           </svg>
         </div>
       </section>
